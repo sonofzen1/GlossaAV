79f9904f542bf59ad580770738c5f8b06aba5242
sonofzen1 <42648223+sonofzen1@users.noreply.github.com>
2025-02-15 15:17:11 -0600

    Added buttons to deck object

--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'card-deck',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './deck.component.html',
   styleUrl: './deck.component.css'
 })
