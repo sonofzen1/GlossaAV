61bd4c89b88988456339ea0c31b116dc66ba29f0
sonofzen1 <42648223+sonofzen1@users.noreply.github.com>
2025-02-17 16:48:24 -0600

    made the flashcard objects look neater

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Flashcard } from '../models/flashcard.model';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-flashcard',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './flashcard.component.html',
   styleUrl: './flashcard.component.css'
 })
