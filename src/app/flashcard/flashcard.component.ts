590e107c3ebbd4a0a6f02134865e67c35f27adbd
sonofzen1 <42648223+sonofzen1@users.noreply.github.com>
2025-03-31 11:40:08 -0500

    GLOS-14

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -2,13 +2,24 @@ import { Component, Input } from '@angular/core';
 import { Flashcard } from '../models/flashcard.model';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faT, faTrash} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-flashcard',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, FontAwesomeModule],
   templateUrl: './flashcard.component.html',
   styleUrl: './flashcard.component.css'
 })
 export class FlashcardComponent {
   @Input() card!: Flashcard;
+  faTrash = faTrash;
+
+  delete(){
+    console.log('Delete button clicked for card:', this.card); // Debug statement
+    // Logic to delete the card can be implemented here
+  }
+
 }
+
+// pr linking to be deleted 
